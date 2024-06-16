2d954e6819feb2ef515d93a3f2aa9bf9bb9192a8
Porsche <herskorn@gmail.com>
2024-06-16 11:52:44 -0700

    added babel loader preset and plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -7,44 +7,44 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js"
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       //Generate HTML File and Inject Bundles
       new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'Text Editor'
+        template: "./index.html",
+        title: "Text Editor"
       }),
 
       //Custom Service Worker
       new InjectManifest ({
-        swSrc: './src-sw.js',
-        swDest: 'src-sw.js'
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js"
       }),
 
       //Manifest PWA Configuration .JSON File
       new WebpackPwaManifest ({
         fingerprints: false,
         inject: true,
-        name: 'Text Editor',
-        short_name: 'Text',
-        description: 'A place to keep your notes.',
-        background_color: '#225ca3',
-        theme_color: '#225ca3',
-        start_url: '/',
-        publicPath: '/',
+        name: "Text Editor",
+        short_name: "Text",
+        description: "A place to keep your notes.",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "/",
+        publicPath: "/",
         icons: [
           {
-            src: path.resolve('src/images/logo.png'),
+            src: path.resolve("src/images/logo.png"),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons')
+            destination: path.join("assets", "icons")
           }
         ]
       })
@@ -55,18 +55,21 @@ module.exports = () => {
         //CSS Loader to Load CSS Files and Style Loader to Inject CSS into DOM
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader']
+          use: ["style-loader", "css-loader"]
         },
         //Babel-Loader to Use ES6
         {
           test: /\.m?js$/,
           exclude: /node_mondules/,
           use: {
-            loader: 'babel-loader',
-            options: [
-              '@babel/plugin-proposal-object-rest-spread',
-              '@babel/transform-runtime',
-            ]
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ]
+            }
           }
         }
       ],
